97ee68567150a5037d23be3b12174f79b3cc516a
joao <jsilvapinheiro403@gmail.com>
2024-05-05 17:41:20 -0300

    projeto pet

---
+++ script.js
@@ -0,0 +1,8 @@
+/*alerta com a mensagem */
+/*alert("Seja bem vindo ao site");
+
+/* função que faz trocar a da cor da imagem no body 
+function trocar(cor) {
+  document.body.style.background = cor;
+}
+*/
